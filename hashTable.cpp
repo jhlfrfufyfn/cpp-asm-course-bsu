ee9870bad8ea90d6ee8a1448b5f27185580dd414
jhlfrfufyfn <55747135+jhlfrfufyfn@users.noreply.github.com>
2019-11-11 11:49:49 +0300

    Update hashTable.cpp

--- hashTable.cpp
+++ hashTable.cpp
@@ -6,66 +6,89 @@
 #include <iterator>
 using namespace std;
 ///============================================================================================================================
-template<typename keyType,typename valueType>
-class HashNode{
+typedef string keyType;
+typedef int valueType;
+class HashNode {
 public:
-	HashNode():_key(0),_value(0),_next(nullptr){}
-	HashNode(const keyType &k, const valueType &v):_key(k),_value(v),_next(nullptr){}
-	keyType& getKey()const;
-	valueType& getValue()const;
-	HashNode* getNext()const;
-	void setKey(const keyType& key);
-	void setValue(const valueType& value);
-	void setNext(const nextType& next);
+	HashNode() :_key(0), _value(0), _next(nullptr) {}
+	HashNode(const keyType &k, const valueType &v) :_key(k), _value(v), _next(nullptr) {}
+
+	keyType getKey()const {
+		return _key;
+	}
+	
+	valueType getValue()const {
+		return _value;
+	}
+
+	HashNode* getNext()const {
+		return _next;
+	}
+
+	void setKey(const keyType& key) {
+		_key=key;
+	}
+
+	void setValue(const valueType& value) {
+		_value=value;
+	}
+	
+	void setNext(HashNode *next) {
+		_next=next;
+	}
+
 private:
 	keyType _key;
 	valueType _value;
 	HashNode *_next;
-	HashNode(const HashNode&);
-	HashNode& operator=(const HashNode&);
-	void copy();
-	void swap();
-	void deleteNext(HashNode* p);
 };
-void HashNode::copy(){
-	
-}
-void HashNode::deleteNext(HashNode *&p){
-	if(p->next)
-		deleteNext(p->next);
-	delete p;
-}
-HashNode::HashNode(const HashNode &other){
-	deleteNext(next);
-	n
-}
-keyType& HashNode::getKey()const{
-	return _key;
-}
-valueType& HashNode::getValue()const{
-	return _value;
-}
-HashNode* HashNode::getNext()const{
-	return _next;
-}
-void HashNode::setKey(const keyType& key){
-	_key=key;
-}
-void HashNode::setValue(const valueType& value){
-	_value=value;
-}
-void HashNode::setNext(HashNode* next){
-	_next=next;
-}
+
 ///=============================================================================================================================
-template<typename keyType,typename valueType>
+
 class HashTable {
 public:
-	HashTable();
-	~HashTable();
-	HashTable& operator=(HashTable& other);
-	valueType& operator [](const keyType& key);
-	HashNode& operator =(const keyType& key);
+	///constructor
+	HashTable():size(0) {
+		initNodes();
+	}
+	///constructor with another hash table
+	HashTable(const HashTable& other) {
+		HashTable();
+		copy(other);
+	}
+
+	///destructor
+	~HashTable() { clear(); }
+	
+	///overloaded operator= 
+	HashTable& operator=(const HashTable& other) {
+		if (this == &other) {
+			return *this;
+		}
+		HashTable t(other);
+		swap(t);
+		return t;
+	}
+	/*
+	valueType& operator[](const keyType& key) {
+		int hash=getHashByKey(key);
+		HashNode *p=_nodes[hash];
+		while (p) {
+			p=p->getNext();
+		}
+		p=new HashNode();
+		p->setKey(key);
+		re
+	}
+
+	valueType operator[](const keyType& key)const {
+		int hash = getHashByKey(key);
+		HashNode *p = _nodes[hash];
+		while (p) {
+			p = p->getNext();
+		}
+		p->setNext(new HashNode())
+	}*/
 	//istream& operator>>(istream& in, );
 	//ostream& operator<<(ostream& out, );
 	bool isEmpty();
@@ -74,11 +97,65 @@ public:
 	int erase();
 	void swap(HashTable& other);
 private:
-	const int TABLE_SIZE=1000;
-	vector<HashNode<keyType,valueType>> nodes;
+	const int TABLE_SIZE = 1000;
+	const int PRIME=237;
+	vector<HashNode*> _nodes;
+	unsigned int size;
+
+	///auxiliary function for constructor
+	void initNodes() {
+		_nodes.resize(TABLE_SIZE,nullptr);
+	}
+
+	///function that makes *this a copy of given hashtable
+	void copy(const HashTable& ht) {
+		for (int i = 0;i < TABLE_SIZE;i++) {
+			if (ht._nodes[i] != nullptr) {
+				HashNode *pHT=ht._nodes[i];
+				HashNode *pThis=this->_nodes[i];
+				while (pHT) {
+					pThis=new HashNode(pHT->getKey(),pHT->getValue());
+					pHT=pHT->getNext();
+					pThis=pThis->getNext();
+				}
+			}
+		}
+	}
+	
+	void clear() {
+		for (int i = 0;i < TABLE_SIZE;i++) {
+			HashNode *p=_nodes[i];
+			while (p) {
+				HashNode *t=p;
+				p=p->getNext();
+				delete t;
+			}
+		}
+	}
+
+	void swap(HashTable& other) {
+		swap(size,other.size);
+		for (int i = 0;i < TABLE_SIZE;i++) {
+			swap(_nodes[i],other._nodes[i]);
+		}
+	}
+
+	int getHashByKey(keyType const& key)const {
+		int hash=0;
+		int currPrime=1;
+		for (auto it : key) {
+			hash+=it*currPrime;
+			currPrime*=currPrime;
+			hash%=TABLE_SIZE;
+			currPrime%=TABLE_SIZE;
+		}
+		return hash;
+	}
+
 };
+
 ///=============================================================================================================================
 
-int main(){
-	
+int main() {
+
 }
