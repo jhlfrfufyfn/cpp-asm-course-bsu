6b9d2b775b5dc878e85a0b8eda95422490f8dd42
jhlfrfufyfn <55747135+jhlfrfufyfn@users.noreply.github.com>
2019-11-18 11:08:39 +0300

    Update RationalNumberClass.cpp

--- RationalNumberClass.cpp
+++ RationalNumberClass.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 class RNum {
 private:
-	long long fNom;
-	long long fDenom;
-	long long __gcd(long long a, long long b) {
+	int fNom;
+	int fDenom;
+	int __gcd(int a, int b) {
 		while (b) {
 			a %= b;
 			swap(a, b);
@@ -14,7 +14,7 @@ private:
 		return a;
 	}
 	void Reduce() {
-		long long gcd = __gcd(abs(fNom), abs(fDenom));
+		int gcd = __gcd(abs(fNom), abs(fDenom));
 		this->fNom /= gcd;
 		this->fDenom /= gcd;
 	}
@@ -30,77 +30,190 @@ private:
 		Reduce();
 	}
 public:
-	explicit RNum(long long f1 = 0, long long f2 = 1) :fNom(f1), fDenom(f2) {this->Normalize();}
-	RNum operator+(RNum const& r2)const {
+
+	explicit RNum(int f1 = 0, int f2 = 1) :fNom(f1), fDenom(f2) { 
+		if (fDenom == 0) {
+			throw invalid_argument("Denominator equals zero!");
+		}
+		this->Normalize(); 
+	}
+
+	int& Numerator() { return fNom; }
+	const int& Numerator()const{ return fNom; }
+	int& Denominator(){ return fDenom; }
+	const int& Denominator() const{ return fDenom; }
+	
+	explicit operator double()const{ return (double)fNom/fDenom; }
+
+	inline const RNum operator+(RNum const& r2)const {
 		RNum t = RNum(this->fNom * r2.fDenom + this->fDenom * r2.fNom, this->fDenom * r2.fDenom);
 		t.Normalize();
 		return t;
 	}
-	RNum operator-(RNum const& r2)const {
+
+	inline const RNum operator-(RNum const& r2)const {
 		RNum t = RNum(this->fNom * r2.fDenom - this->fDenom * r2.fNom, this->fDenom * r2.fDenom);
 		t.Normalize();
 		return t;
 	}
-	RNum operator*(RNum const& r2)const {
+
+	inline const RNum operator*(RNum const& r2)const {
 		RNum t = RNum(this->fNom * r2.fNom, this->fDenom * r2.fDenom);
 		t.Normalize();
 		return t;
 	}
-	RNum operator/(RNum const& r2)const {
+
+	inline const RNum operator/(RNum const& r2)const {
 		RNum t = RNum(this->fNom * r2.fDenom, this->fDenom * r2.fNom);
 		t.Normalize();
 		return t;
 	}
-	RNum& operator+=(RNum const& r2) {
-		*this=*this+r2;
+
+	inline const RNum operator+(int r2)const {
+		RNum t=*this+RNum(r2);
+		t.Normalize();
+		return t;
+	}
+
+	inline const RNum operator-(int r2)const {
+		RNum t = *this-RNum(r2);
+		t.Normalize();
+		return t;
+	}
+
+	inline const RNum operator*(RNum const& r2)const {
+		RNum t = *this * RNum(r2);
+		t.Normalize();
+		return t;
+	}
+
+	inline const RNum operator/(RNum const& r2)const {
+		RNum t = *this / RNum(r2);
+		t.Normalize();
+		return t;
+	}
+
+	inline RNum& operator=(RNum const& r2) {
 		return *this;
 	}
-	RNum& operator-=(RNum const& r2) {
+
+	inline RNum& operator+=(RNum const& r2) {
+		*this = *this + r2;
+		return *this;
+	}
+
+	inline RNum& operator-=(RNum const& r2) {
 		*this = *this - r2;
 		return *this;
 	}
-	RNum& operator*=(RNum const& r2) {
+
+	inline RNum& operator*=(RNum const& r2) {
 		*this = *this * r2;
 		return *this;
 	}
-	RNum& operator/=(RNum const& r2) {
+
+	inline RNum& operator/=(RNum const& r2) {
 		*this = *this / r2;
 		return *this;
 	}
+
+	inline RNum& operator+=(int r2) {
+		*this = *this + RNum(r2);
+		return *this;
+	}
+
+	inline RNum& operator-=(int r2) {
+		*this = *this - RNum(r2);
+		return *this;
+	}
+
+	inline RNum& operator*=(int r2) {
+		*this = *this * RNum(r2);
+		return *this;
+	}
+
+	inline RNum& operator/=(int r2) {
+		*this = *this / RNum(r2);
+		return *this;
+	}
+
 	bool const operator<(RNum const& r2)const {
-		long long comDenom=this->fDenom*r2.fDenom;
-		long long thisNom=this->fNom*r2.fDenom;
-		long long r2Nom=r2.fNom*this->fDenom;
-		if(thisNom<r2Nom)
+		long long comDenom = this->fDenom*r2.fDenom;
+		long long thisNom = this->fNom*r2.fDenom;
+		long long r2Nom = r2.fNom*this->fDenom;
+		if (thisNom < r2Nom)
 			return 1;
 		else
 			return 0;
 	}
+
 	bool const operator==(RNum const& r2)const {
-		if(this->fNom==r2.fNom&&this->fDenom==r2.fDenom)
+		if (this->fNom == r2.fNom&&this->fDenom == r2.fDenom)
 			return 1;
 		else
 			return 0;
 	}
+
 	bool const operator>(RNum const& r2)const {
-		if((!(*this<r2))&&(!(*this==r2)))
+		if ((!(*this < r2)) && (!(*this == r2)))
 			return 1;
 		else
 			return 0;
 	}
+
 	friend ostream& operator<<(ostream &out, const RNum& x) {
-		out << x.fNom << "/" << x.fDenom;
+		out << x.fNom;
+		if(x.fDenom!=1)
+			out << "/" << x.fDenom;
 		return out;
 	}
+
+	friend istream& operator >> (istream &in, RNum& r) {
+		int a;
+		in>>a;
+		r.Numerator()=a;
+		in.ignore(1);
+		in>>a;
+		r.Denominator()=a;
+		return in;
+	}
+
+
 };
+
+inline KRat const  operator + (int a, KRat const& r2)
+{
+	return KRat(a * r2.fDenom + r2.fNum, r2.fDenom);
+}
+
+inline KRat  operator * (int a, const KRat& r2)
+{
+	return KRat(a*r2.fNum, r2.fDenom);
+}
+
+inline KRat const  operator - (int a, KRat const& r2)
+{
+	return KRat(a * r2.fDenom + r2.fNum, r2.fDenom);
+}
+
+inline KRat const  operator / (int a, KRat const& r2)
+{
+	return KRat(a * r2.fDenom + r2.fNum, r2.fDenom);
+}
+
+
 int main() {
-	RNum A(1,3),B(15,3);
-	cout<<"A+B= "<<A+B<<"\n";
-	cout<<"A*B= "<<A*B<<"\n";
-	cout<<"A/B= "<<A/B<<"\n";
-	cout<<"A<B= "<<(A<B)<<"\n";
-	cout<<"A=B= "<<(A==B)<<"\n";
-	cout<<"A>B= "<<(A>B)<<"\n";
+	RNum A, B;
+	cin>>A>>B;
+	cout << "A+B= " << A + B << "\n";
+	cout << "A*B= " << A*B << "\n";
+	cout << "A/B= " << A / B << "\n";
+	cout << "A<B= " << (A < B) << "\n";
+	cout << "A=B= " << (A == B) << "\n";
+	cout << "A>B= " << (A > B) << "\n";
+	A = B;
+	cout << "A= " << A << "\n";
+	cout << "B= " << B << "\n";
 	system("pause");
 	return 0;
 }
