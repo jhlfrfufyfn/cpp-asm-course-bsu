653fc3faa591827a05e891b9a36e92260782bb7f
jhlfrfufyfn <55747135+jhlfrfufyfn@users.noreply.github.com>
2019-11-22 09:15:58 +0300

    Update swapUpperDiagonals.cpp

--- swapUpperDiagonals.cpp
+++ swapUpperDiagonals.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 using namespace std;
 typedef long long ll;
-
-void swapTask(int **a,int n,int m) {
+///_delspec naked
+void swapTask(int **a, int n, int m) {
 	__asm {
 		/*
 		mov ebx,a;
@@ -21,13 +21,13 @@ void swapTask(int **a,int n,int m) {
 		///цикл по строкам
 		///цикл по столбцам
 		///поменять в строке i элементы a[i][i] и a[i][n-i-1]
-		mov eax,0;///using for swap
-		mov ebx,0;///adress
-		mov ecx,n;///counter of a row
-		mov edx,0;///shift of a column
-		mov esi,4;///shift of a row;
-		mov ebx,a;
-		mov edi,0;
+		mov eax, 0;///using for swap
+		mov ebx, 0;///adress
+		mov ecx, n;///counter of a row
+		mov edx, 0;///shift of a column
+		mov esi, 4;///shift of a row;
+		mov ebx, a;
+		mov edi, 0;
 		mov ebx, a;
 		mov ebx, [ebx];
 	loop1:
@@ -35,52 +35,52 @@ void swapTask(int **a,int n,int m) {
 		mov edx, 0;
 		mov eax, 0;
 		///calculate current second index;
-		mov ecx,m;
-		sub ecx,1;
-		sub ecx,edi;
+		mov ecx, m;
+		sub ecx, 1;
+		sub ecx, edi;
 		///
-		mov edx,[ebx][edi*4];///t=a;
-		mov eax,[ebx][ecx*4];///a=b;
-		mov [ebx][edi*4],eax;///a=b;
-		mov [ebx][ecx*4],edx;///b=t;
+		mov edx, [ebx][edi * 4];///t=a;
+		mov eax, [ebx][ecx * 4];///a=b;
+		mov[ebx][edi * 4], eax;///a=b;
+		mov[ebx][ecx * 4], edx;///b=t;
 		cmp ecx, 0;
 		pop ecx;
 		jle exitCycle;
 		inc edi;
 		mov ebx, a;
-		add ebx,esi;
+		add ebx, esi;
 		mov ebx, [ebx];
-		add esi,4;
-	loop loop1;
+		add esi, 4;
+		loop loop1;
 	exitCycle:
 		nop;
 	}
 }
 
 signed main() {
-	cout<<"Enter n: ";
+	cout << "Enter n: ";
 	int n;
-	cin>>n;
-	cout<<"Enter m: ";
+	cin >> n;
+	cout << "Enter m: ";
 	int m;
-	cin>>m;
+	cin >> m;
 	srand(329);
-	int **a=new int*[n];
+	int **a = new int*[n];
 	for (int i = 0;i < n;i++) {
-		a[i]=new int[m];
+		a[i] = new int[m];
 	}
 	for (int i = 0;i < n;i++) {
 		for (int j = 0;j < m;j++) {
-			a[i][j]=rand()%10;
+			a[i][j] = rand() % 10;
 		}
 	}
-	cout<<endl;
+	cout << endl;
 	for (int i = 0;i < n;i++) {
-		for(int j=0;j<m;j++)
-			cout<<a[i][j]<<" ";
-		cout<<endl;
+		for (int j = 0;j<m;j++)
+			cout << a[i][j] << " ";
+		cout << endl;
 	}
-	swapTask(a,n,m);	
+	swapTask(a, n, m);
 	cout << endl;
 	for (int i = 0;i < n;i++) {
 		for (int j = 0;j<m;j++)
@@ -90,4 +90,3 @@ signed main() {
 	system("pause");
 	return 0;
 }
-
