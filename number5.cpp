2d78e8b2045a6fac5e0a36b12d0d236fe6d11232
jhlfrfufyfn <55747135+jhlfrfufyfn@users.noreply.github.com>
2019-12-01 18:13:00 +0300

    Update number5.cpp

--- number5.cpp
+++ number5.cpp
@@ -2,8 +2,7 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-///(1,1) (1,0) (0,x)
-int funcP1(int n, int x,int currFi1,int currFi2) {
+int funcP1(int n, int x, int currFi1=1, int currFi2=1) {
 	__asm {
 		///initialize x^n +
 		///initialize f0,f1 +
@@ -13,6 +12,22 @@ int funcP1(int n, int x,int currFi1,int currFi2) {
 		///unused: ebx, edi,esi
 		push esi;
 		push edi;
+		cmp n, 1;
+		jne NOT11;
+		cmp x, 1;
+		jne NOT11;
+		jmp MARK8;
+	NOT11:
+		cmp n,1;
+		jne NOT10;
+		cmp x,0;
+		jne NOT10;
+		jmp MARK6;
+	NOT10:
+		cmp n,0;
+		jne NOT0X;
+		jmp MARK6;
+	NOT0X:
 		mov ebx, 0;
 		mov edi, 1;
 		mov currFi1, 1;
@@ -27,7 +42,7 @@ int funcP1(int n, int x,int currFi1,int currFi2) {
 		mov eax, edi;
 		mov ecx, x;
 		mov edx, 0;
-		cmp ecx,0;
+		cmp ecx, 0;
 		je MARK1;
 		div ecx;
 	MARK1:
@@ -47,7 +62,7 @@ int funcP1(int n, int x,int currFi1,int currFi2) {
 		mov ecx, x;
 		mov eax, edi;
 		mov edx, 0;
-		cmp ecx,0;
+		cmp ecx, 0;
 		je MARK2;
 		div ecx;
 	MARK2:
@@ -62,22 +77,27 @@ int funcP1(int n, int x,int currFi1,int currFi2) {
 		add esi, ecx;
 		pop ecx;
 		pop currFi1;
-		cmp ecx,0;
+		cmp ecx, 0;
 		je MARK4;
 		loop c2;
 	MARK4:
-		cmp x,0;
+		cmp x, 0;
 		je MARK6;
 		mov eax, ebx;
 		jmp MARK7;
 	MARK6:
+		mov eax, currFi1;
+		jmp MARK7;
+	MARK8:
 		mov eax,currFi1;
+		add eax,currFi2;
+		jmp MARK7;
 	MARK7:
 		pop edi;
 		pop esi;
 	}
 }
-int funcP2(int n, int x, int currFi1, int currFi2) {
+int funcP2(int n, int x, int currFi1=1, int currFi2=1) {
 	__asm {
 		///initialize x^0 +
 		///initialize f0,f1 +
@@ -85,8 +105,10 @@ int funcP2(int n, int x, int currFi1, int currFi2) {
 		///write a cycle +	
 		push esi;
 		push edi;
-		cmp n,1;
+		cmp n, 1;
 		je MARK6;
+		cmp n,0;
+		je MARK8;
 		mov ebx, 0;
 		mov edi, 1;
 		mov currFi1, 1;
@@ -124,15 +146,19 @@ int funcP2(int n, int x, int currFi1, int currFi2) {
 		add esi, ecx;
 		pop ecx;
 		pop currFi1;
-		cmp ecx,0;
+		cmp ecx, 0;
 		je MARK5;
 		loop c2;
 	MARK5:
 		mov eax, ebx;
 		jmp MARK7;
 	MARK6:
-		mov eax,1;
-		add eax,x;
+		mov eax, 1;
+		add eax, x;
+		jmp MARK7;
+	MARK8:
+		mov eax,currFi1;
+		jmp MARK7;
 	MARK7:
 		pop edi;
 		pop esi;
@@ -143,7 +169,7 @@ int checkCpp1(int n, int x) {
 	int currX = 1;
 	vector<int> f(n + 1);
 	f[0] = 1;
-	if(n!=0)
+	if (n != 0)
 		f[1] = 1;
 	for (int i = 2;i <= n;i++) {
 		f[i] = f[i - 1] + f[i - 2];
@@ -159,8 +185,8 @@ int checkCpp2(int n, int x) {
 	int currX = 1;
 	vector<int> f(n + 1);
 	f[0] = 1;
-	if(n!=0)
-	f[1] = 1;
+	if (n != 0)
+		f[1] = 1;
 	for (int i = 2;i <= n;i++) {
 		f[i] = f[i - 1] + f[i - 2];
 	}
@@ -175,10 +201,10 @@ int main()
 	int n, x;
 	cout << "Enter n,x:\n ";
 	cin >> n >> x;
-	if (checkCpp1(n, x) == funcP1(n, x,1,1) && checkCpp2(n, x) == funcP2(n, x,1,1))
+	if (checkCpp1(n, x) == funcP1(n, x) && checkCpp2(n, x) == funcP2(n, x))
 		cout << "Ok\n";
-	cout << "p1 = " << checkCpp1(n, x) << "\np2 = " << checkCpp2(n, x) << "\n";
-	cout << "p1 = " << funcP1(n, x,1,1) << "\np2 = " << funcP2(n, x,1,1) << "\n";
+	cout << "p1Correct = " << checkCpp1(n, x) << "\np2Correct = " << checkCpp2(n, x) << "\n";
+	cout << "p1 = " << funcP1(n, x) << "\np2 = " << funcP2(n, x) << "\n";
 	system("pause");
 	return 0;
 }
