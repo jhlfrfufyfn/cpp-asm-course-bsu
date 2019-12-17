1671e1c0b7f4b8ef328786625a5e6328b24479a0
jhlfrfufyfn <55747135+jhlfrfufyfn@users.noreply.github.com>
2019-12-17 14:54:22 +0300

    Update chainAsm.cpp

--- chainAsm.cpp
+++ chainAsm.cpp
@@ -2,28 +2,64 @@
 #include <iostream>
 using namespace std;
 ///32
-int FWords(char* s) {
+int FWords(const char* s) {
 	__asm {
 		mov edi, s;
 		mov ax, 0;
 		mov es, ax;
 		mov ax, 32;
-		mov ecx, 100;
-		cld;	
-		repne scas s;
-		je found;
-	found:
-		mov eax, edi;
-		sub eax, s;
-		jmp exit;
-		mov eax, -1;
-	exit:
+		mov ecx, 100000;
+		mov edx, 0; /// counter of the words
+		cmp [edi], 32;
+		je SKIPIF;
+		inc edx;
+		scasb;
+	SKIPIF:
+		for1:
+			cld; 
+			repne scasb;
+			cmp [edi-1], 32;
+			dec edi;
+			cmp [edi - 1], 32;
+			je init1;
+		init1: 
+			mov bl, 1;///flag for left letter
+			jmp further;
+		init0:
+			mov bl, 0;
+			jmp further;
+		further:
+			cmp [edi + 1], 32;	
+			je init21;
+		init21:
+			mov bh, 1;///flag for right letter
+			jmp further1;
+		init20:
+			mov bh, 0;
+			jmp further1;
+		further1:
+			cmp bh, 32;
+			je SKIPIF2;
+			cmp[edi + 1], 'a';
+			jl SKIPIF2;
+			cmp[edi + 1], 'z';
+			jg SKIPIF2;
+			inc edx;
+		SKIPIF2:
+		endofloop:
+			inc edi;
+		loop for1;
+	exit1:
+		mov eax, edx;
 	}
 }
-
+const string test1 = "a a a";
+const string test2 = "a a a";
+const string test3 = "a a a";
+const string test4 = "a a a";
 int main() {
-	char *s = "Length 8";
-	cout<< FWords(s) <<"\n";
+	const char *s = test1.c_str();
+	cout << FWords(s) << "\n";
 	system("pause");
 	return 0;
 }
