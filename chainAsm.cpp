3705a21a7f019499bcba9a196f0ae13b39fb981d
jhlfrfufyfn <55747135+jhlfrfufyfn@users.noreply.github.com>
2019-12-20 13:50:06 +0300

    Update chainAsm.cpp

--- chainAsm.cpp
+++ chainAsm.cpp
@@ -1,65 +1,51 @@
 #include <iomanip>
 #include <iostream>
+#include "gtest/gtest.h"
 using namespace std;
-///32
-int FWords(const char* s) {
-	__asm {
-		mov edi, s;
-		mov ax, 0;
-		mov es, ax;
-		mov ax, 32;
-		mov ecx, 100000;
-		mov edx, 0; /// counter of the words
-		cmp [edi], 32;
-		je SKIPIF;
-		inc edx;
+int FWords(char* s)
+{
+	_asm
+	{
+		push ds;
+		pop es;
+		mov ecx, 0;
+		mov ebx, 0;
+		cld;
+		lea edi, s;
+		mov edi, [edi];
+		mov al, 0;
+	for1:
 		scasb;
-	SKIPIF:
-		for1:
-			cld; 
-			repne scasb;
-			cmp [edi-1], 32;
-			dec edi;
-			cmp [edi - 1], 32;
-			je init1;
-		init1: 
-			mov bl, 1;///flag for left letter
-			jmp further;
-		init0:
-			mov bl, 0;
-			jmp further;
-		further:
-			cmp [edi + 1], 32;	
-			je init21;
-		init21:
-			mov bh, 1;///flag for right letter
-			jmp further1;
-		init20:
-			mov bh, 0;
-			jmp further1;
-		further1:
-			cmp bh, 32;
-			je SKIPIF2;
-			cmp[edi + 1], 'a';
-			jl SKIPIF2;
-			cmp[edi + 1], 'z';
-			jg SKIPIF2;
-			inc edx;
-		SKIPIF2:
-		endofloop:
-			inc edi;
-		loop for1;
+		je exit1;
+		inc ecx;
+		jmp for1;
 	exit1:
-		mov eax, edx;
+		inc ecx;
+			lea edi, s;
+			mov edi, [edi];
+			mov al, ' ';
+			for2 :
+			cmp ecx, 1;
+			je exitt;
+		repe scasb
+			dec edi;
+		cmp es:[edi],0;
+		je exitt;
+		inc ecx;
+		inc ebx;
+		repne scasb;
+		dec edi;
+		inc ecx;
+		jmp for2;
+	exitt:
+		cld;
+	mov eax, ebx;
 	}
 }
-const string test1 = "a a a";
-const string test2 = "a a a";
-const string test3 = "a a a";
-const string test4 = "a a a";
+
 int main() {
-	const char *s = test1.c_str();
-	cout << FWords(s) << "\n";
+	char *ch = " a a ";
+	std::cout << FWords(ch);
 	system("pause");
 	return 0;
 }
